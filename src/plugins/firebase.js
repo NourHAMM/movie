9b9b1efa840e55a68f20eb87b8cd2ab92ee69045
NourHAMM <60269159+NourHAMM@users.noreply.github.com>
2020-03-26 09:08:02 +0400

    fixed firebase

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -4,13 +4,13 @@ import "firebase/firebase-auth";
 import * as firebaseui from "firebaseui";
 
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_FIREBASE_apiKey,
-  authDomain: process.env.VUE_APP_FIREBASE_authDomain,
-  databaseURL: process.env.VUE_APP_FIREBASE_databaseURL,
-  projectId: process.env.VUE_APP_FIREBASE_projectId,
-  storageBucket: process.env.VUE_APP_FIREBASE_storageBucket,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_messagingSenderId,
-  appId: process.env.VUE_APP_FIREBASE_appId
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId
 };
 firebase.initializeApp(firebaseConfig);
 const ui = new firebaseui.auth.AuthUI(firebase.auth());
