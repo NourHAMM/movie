4393b0988398e727bc6a82eca91897a99467e1c3
NourHAMM <60269159+NourHAMM@users.noreply.github.com>
2020-03-29 15:23:37 +0400

    fb

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 axios.defaults.baseURL = process.env.VUE_APP_API_url
 
-axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.VUE_APP_API_accessToken}`
+axios.defaults.headers.common['Authorization'] = `Bearer ${ process.env.VUE_APP_API_accessToken }`
 
 
 
