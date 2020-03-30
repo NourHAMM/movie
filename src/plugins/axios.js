123fce553960bfaf161e730e124efbfbca25e481
NourHAMM <60269159+NourHAMM@users.noreply.github.com>
2020-03-30 08:39:00 +0400

    fb

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 
 import axios from 'axios'
+axios.defaults.baseURL = process.env.VUE_APP_API_url
 
-axios.defaults.baseURL = "https://api.themoviedb.org/3"
-
-axios.defaults.headers.common['Authorization'] = `Bearer ${ "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZDA5MGY3MjgwZDBhODY2MjExYjU0NjU1M2YyM2M0YyIsInN1YiI6IjVlN2IzYzA5NjNkNzEzMDAxMjhiMmFlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Pqd8jKi5OQL7K2CIIEfO2VLbQc1XBDLc5GQYpV3M0lE"
-}`
-
+axios.defaults.headers.common['Authorization'] = `Bearer ${ process.env.VUE_APP_API_accessToken }`
 
 
 Vue.prototype.$axios = axios
